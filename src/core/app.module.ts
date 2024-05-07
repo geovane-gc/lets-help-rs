02abfcd79967b27b2f3c9cf4c5f7c3d9f3b23a95
Geovane Gallotti Cazal <geovanegallotticazal@hotmail.com>
2024-05-06 23:47:50 -0300

    add colecct point module in app module

--- src/core/app.module.ts
+++ src/core/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from './config/config.module';
+import { CollectPointModule } from 'src/modules/collect-point/collect-point.module';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [ConfigModule, CollectPointModule],
   controllers: [],
   providers: [],
 })
