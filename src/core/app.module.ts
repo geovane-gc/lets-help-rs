ccf2eba1a64b272680d83349e88e21372ce8d882
João Augusto Perin de Souza <joaoaugustoperin@gmail.com>
2024-05-08 14:40:58 -0300

    chore: add throttling

--- src/core/app.module.ts
+++ src/core/app.module.ts
@@ -1,11 +1,29 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { CollectPointModule } from 'src/modules/collect-point/collect-point.module';
 import { LocationModule } from 'src/modules/location/location.module';
 import { ConfigModule } from './config/config.module';
+import { ConfigService } from './config/config.service';
 
 @Module({
-  imports: [ConfigModule, CollectPointModule, LocationModule],
+  imports: [
+    ConfigModule,
+    ThrottlerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) =>
+        configService.configureThrottler(),
+    }),
+    CollectPointModule,
+    LocationModule,
+  ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
